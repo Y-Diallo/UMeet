2e44a2e84d48db14b89f14324e78d4458268dfa8
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2023-05-05 16:38:02 -0700

    modified strucutre

--- UMeet/src/Root.tsx
+++ UMeet/src/Root.tsx
@@ -1,27 +1,15 @@
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Example from "./Example";
 import { createContext, useState } from "react";
-import LoginSignUp from "./Routes/Landing/LoginSignUp";
+import LoginSignUp from "./Routes/LoginSignUp/LoginSignUp";
 import Landing from "./Routes/Landing/Landing";
-import Home from "./Routes/Landing/Home";
+import Home from "./Routes/Home/Home";
 
 export const userContext = createContext<{user: any | null, setUser: (user: any | null) => void}>({user: null, setUser: () => {}});
 
 
 function Root() {
   const [user, setUser] = useState<any | null>(null);
-  // if(user == null){
-  //   const userString = localStorage.getItem("user");
-  //   if(userString != null){
-  //     setUser(JSON.parse(userString));
-  //   }
-  //   if(userString == null && window.location.pathname != "/"){
-  //     setUser(null);
-  //   }
-  // }
-  // if(user != null){
-  //   localStorage.setItem("user", JSON.stringify(user));
-  // }
 
     return ( 
     <userContext.Provider value={{user, setUser}}>
@@ -30,12 +18,12 @@ function Root() {
           <Route path="*" element={<Landing/>}/>
           <Route path="/login" element={<LoginSignUp/>}/>
           <Route path="/home" element={user == null? <Landing/>:<Home/>}/>
-          <Route path="/search" element={<Example/>}/>
-          <Route path="/profile/:userId" element={<Example/>}/>
-          <Route path="/profile/:userId/edit" element={<Example/>}/>
-          <Route path="/events" element={<Example/>}/>
-          <Route path="/create_event" element={<Example/>}/>
-          <Route path="/event_details/:eventId" element={<Example/>}/>
+          <Route path="/search" element={user == null? <Landing/>:<Example/>}/>
+          <Route path="/profile/:userId" element={user == null? <Landing/>:<Example/>}/>
+          <Route path="/profile/:userId/edit" element={user == null? <Landing/>:<Example/>}/>
+          <Route path="/events" element={user == null? <Landing/>:<Example/>}/>
+          <Route path="/create_event" element={user == null? <Landing/>:<Example/>}/>
+          <Route path="/event_details/:eventId" element={user == null? <Landing/>:<Example/>}/>
         </Routes>
       </BrowserRouter>
     </userContext.Provider>
--- UMeet/src/Routes/LoginSignUp/LoginSignUp.tsx
+++ UMeet/src/Routes/LoginSignUp/LoginSignUp.tsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import { PRIMARY_COLOR, SECONDARY_COLOR } from "../../scripts/colors.ts";
 
-import SignIn from "./Components/SignIn";
-import SignUp from "./Components/SignUp";
+import SignIn from "./Components/SignIn.tsx";
+import SignUp from "./Components/SignUp.tsx";
 
 function LoginSignUp() {
     const [needSignUp, setNeedSignUp] = useState(false);
 		
     return (
-    <div style={{margin:"auto"}}>
+    <div style={{display:"flex", width:"auto", height:"auto"}}>
         <div
             style={{backgroundColor:SECONDARY_COLOR,
+            flexGrow:1,
             display:"flex",
             flexDirection:"column",
             justifyContent:"center",
             alignItems:"center"}}
         >
             {needSignUp ? <SignUp/> : <SignIn/>}
-            <input style={{color:SECONDARY_COLOR, backgroundColor:PRIMARY_COLOR, maxWidth:"25vw", width:"20em", margin:"2em"}} type="button" value={needSignUp ? "registered?":"not registered?"} onClick={(event) => setNeedSignUp(!needSignUp)}/>
+            <input style={{
+                color:SECONDARY_COLOR,
+                backgroundColor:PRIMARY_COLOR,
+                maxWidth:"50vw", 
+                width:"20em", 
+                margin:"2em"
+            }} type="button" value={needSignUp ? "registered?":"not registered?"} onClick={(event) => setNeedSignUp(!needSignUp)}/>
         </div>
     </div>
     );
