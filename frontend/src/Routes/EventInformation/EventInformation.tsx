8a95652ee48be48723faaf0ce96b3f8380854529
Youssoupha Diallo <youssoupha.d72@gmail.com>
2023-05-06 16:01:19 -0700

    Update EventInformation.tsx

--- frontend/src/Routes/EventInformation/EventInformation.tsx
+++ frontend/src/Routes/EventInformation/EventInformation.tsx
@@ -4,18 +4,31 @@ import { faCalendarDays,faClock } from '@fortawesome/free-regular-svg-icons';
 import { faChevronLeft, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { defaultEvents } from "../../scripts/defaultData";
 import { EventDetails } from "../../scripts/types";
+import { useNavigate } from "react-router-dom";
 
 function EventInformation() {
+    const navigate = useNavigate();
     const dateOptions : Intl.DateTimeFormatOptions = { month: 'long', day: 'numeric' };
     const dayOptions : Intl.DateTimeFormatOptions = { weekday: 'long' };
     const timeOptions : Intl.DateTimeFormatOptions  = { hour: 'numeric', minute: 'numeric', hour12: true };
     const [event, setEvent] = useState<EventDetails>(defaultEvents[0]);
+    const [isEnrolled, setIsEnrolled] = useState<boolean>(true);
+
+    function handleEnroll(): void {
+        throw new Error("Function not implemented.");
+    }
+
+    function openMap(): void {
+        //open google maps with event location
+        const url = `https://www.google.com/maps/search/?api=1&query=${event.location}`;
+        window.open(url, '_blank');
+    }
 
     return ( 
         <div>
               <div className="overlay absolute inset-0 bg-gray-900 opacity-60"></div>
             <div style={{backgroundImage: `url(${event.image2 != undefined ? event.image2 : event.image})`,backgroundSize: 'cover', backgroundPosition: 'center', width:'100vw'}} className="p-0">
-                <FontAwesomeIcon style={{ zIndex: 10, position: 'absolute', top: '10%', left: '10%', color:'white'}} icon={faChevronLeft} onClick={() => window.history.back()}/>
+                <FontAwesomeIcon style={{ zIndex: 10, position: 'absolute', top: '5em', left: '2em', color:'white', margin:"1em"}} icon={faChevronLeft} onClick={() => navigate(-1)}/>
                 <div className="min-h-screen flex flex-col ">
                     <h2 className="font-semibold text-2xl mt-6 text-left text-white"
                     style={{ zIndex: 10, position: 'absolute', top: '30%', left: '40%', transform: 'translate(-50%, -50%)'}}>
@@ -43,7 +56,7 @@ function EventInformation() {
                 </div>
 
                 <div style={{ zIndex: 10, position: 'absolute', top: '50%', left: '60%', display: 'flex', alignItems: 'center'}}>
-                <button className="bg-[#4B2E83] rounded-full px-4 py-2 text-white font-bold">Map</button>
+                <button onClick={()=> openMap()} className="bg-[#4B2E83] rounded-full px-4 py-2 text-white font-bold">Map</button>
                 </div>
 
                 <div style={{ zIndex: 10, position: 'absolute', top: '60%', left: '12%', display: 'flex', alignItems: 'center', textAlign:'left' }}>
@@ -59,7 +72,7 @@ function EventInformation() {
                         <img className="inline-block h-8 w-8 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
                     </div>
                 <div style={{ justifySelf: 'end' }}>
-                    <button className="bg-transparent border-2 border-[#4B2E83] px-3 py-1 text-purple font-bold rounded-full ml-20">
+                    <button onClick={()=> navigator.share({title: event.title, text:event.description, url: window.location.href})} className="bg-transparent border-2 border-[#4B2E83] px-3 py-1 text-purple font-bold rounded-full ml-20">
                     Invite
                     </button>
                 </div>
@@ -72,8 +85,8 @@ function EventInformation() {
 
 
                 <div style={{ zIndex: 10, position: 'absolute', bottom: '0', left: '0', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                    <button className="bg-[#4B2E83] px-8 py-5 text-white font-bold w-full">
-                        Already Enrolled
+                    <button onClick={()=> handleEnroll()} className="bg-[#4B2E83] px-8 py-5 text-white font-bold w-full">
+                        {isEnrolled? "Already Enrolled": "Enroll"}
                     </button>
                 </div>
 
