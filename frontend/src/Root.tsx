b4a410fd44ae4de883536710eec56444df9ec384
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2023-05-05 17:14:43 -0700

    renamed umeet to frontend

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -17,13 +17,27 @@ function Root() {
         <Routes>
           <Route path="*" element={<Landing/>}/>
           <Route path="/login" element={<LoginSignUp/>}/>
-          <Route path="/home" element={user == null? <Landing/>:<Home/>}/>
-          <Route path="/search" element={user == null? <Landing/>:<Example/>}/>
-          <Route path="/profile/:userId" element={user == null? <Landing/>:<Profile/>}/>
-          <Route path="/profile/:userId/edit" element={user == null? <Landing/>:<Example/>}/>
-          <Route path="/events" element={user == null? <Landing/>:<Example/>}/>
-          <Route path="/create_event" element={user == null? <Landing/>:<Example/>}/>
-          <Route path="/event_details/:eventId" element={user == null? <Landing/>:<Example/>}/>
+          <Route path="/home" element={
+            // user == null? <LoginSignUp/>:
+          <Home/>}/>
+          <Route path="/search" element={
+            // user == null? <LoginSignUp/>:
+            <Example/>}/>
+          <Route path="/profile/:userId" element={
+            // user == null? <LoginSignUp/>:
+            <Profile/>}/>
+          <Route path="/profile/:userId/edit" element={
+            // user == null? <LoginSignUp/>:
+            <Example/>}/>
+          <Route path="/events" element={
+            // user == null? <LoginSignUp/>:
+            <Example/>}/>
+          <Route path="/create_event" element={
+            // user == null? <LoginSignUp/>:
+            <Example/>}/>
+          <Route path="/event_details/:eventId" element={
+            // user == null? <LoginSignUp/>:
+            <Example/>}/>
         </Routes>
       </BrowserRouter>
     </userContext.Provider>
