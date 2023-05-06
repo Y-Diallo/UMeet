e71076d8fbf7159a2178af69c57a57c88df0f776
Youssoupha Diallo <youssoupha.d72@gmail.com>
2023-05-06 01:14:48 -0700

    modified navbar

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -1,33 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faCompass, faTicket, faGear } from '@fortawesome/free-solid-svg-icons'
 function Navbar() {
-    return (
-      <nav className="fixed bottom-0 left-0 w-full text-white">
-        <ul className="flex justify-between px-8 py-8">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
-                <FontAwesomeIcon className="text-lg" style={{color:'#B7A57AFF'}} icon={faHouse} />
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">
-                <FontAwesomeIcon className="text-purple-900 text-lg" icon={faCompass} />
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact" className="nav-link">
-                <FontAwesomeIcon className="text-purple-900 text-lg" icon={faTicket} />
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact" className="nav-link">
-                <FontAwesomeIcon className="text-purple-900 text-lg" icon={faGear} />
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    );
+  const page = useLocation().pathname.split('/')[1];
+  return (
+    <nav className="fixed bottom-0 left-0 w-full text-white bg-white">
+      <ul className="flex justify-between px-8 py-8">
+        <li className="nav-item">
+          <Link to="/home" className="nav-link">
+              <FontAwesomeIcon className="text-purple-900 text-lg"
+               style={page == "home" ? {color:'#B7A57AFF'}:{}} icon={faHouse} />
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/search" className="nav-link">
+              <FontAwesomeIcon className="text-purple-900 text-lg"
+              style={page == "search" ? {color:'#B7A57AFF'}:{}} icon={faCompass} />
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/events" className="nav-link">
+              <FontAwesomeIcon className="text-purple-900 text-lg"
+              style={page == "events" ? {color:'#B7A57AFF'}:{}} icon={faTicket} />
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/settings" className="nav-link">
+              <FontAwesomeIcon className="text-purple-900 text-lg"
+              style={page == "settings" ? {color:'#B7A57AFF'}:{}} icon={faGear} />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
   }
   
 
