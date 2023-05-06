0352172a33183b7bcf8d3fac17ce75f8c0d2ab01
DishaMahajan22 <disham2004@gmail.com>
2023-05-06 00:40:05 -0700

    finished nav, added its routing, and fixed search bar

--- frontend/src/Routes/Home/Home.tsx
+++ frontend/src/Routes/Home/Home.tsx
@@ -11,16 +11,20 @@ function Home() {
             <h2 className="font-semibold text-2xl mt-6">Welcome Back</h2>
             <h3 className="leading-7 font-sans text-purple-900 font-normal">Alexander Hoff</h3>
             </div>
-            <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2.25&amp;w=256&amp;h=256&amp;q=80" alt="Profile icon" className="w-10 h-10 ml-auto rounded-full mr-2" />
+            <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2.25&amp;w=256&amp;h=256&amp;q=80" alt="Profile icon" className="w-12 h-12 ml-auto rounded-full mr-2 mt-4" />
         </div>
 
         <label className="relative block mt-6">
-        <span className="sr-only">Search</span>
-        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-            <FontAwesomeIcon icon={faSearch} />
-        </span>
-        <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-full py-3 pl-10 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Music event, Parties..." type="text" name="search"/>
+            <span className="sr-only">Search</span>
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <FontAwesomeIcon icon={faSearch} />
+            </span>
+            <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-full py-3 pl-10 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Music event, Parties..." type="text" name="search"/>
+            <button type="submit" className="absolute top-0 right-0 h-full p-2.5 text-sm font-medium text-white bg-purple-900 rounded-r-full border border-purple-900 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
+                Search
+            </button>
         </label>
+
 
         <h3 className="leading-7 font-sans font-normal mt-8">Event Categories</h3>
 
