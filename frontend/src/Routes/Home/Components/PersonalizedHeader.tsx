96fc1b65bc0bff746c17f5e4b98ec1e5b8ed80e8
Youssoupha Diallo <youssoupha.d72@gmail.com>
2023-05-07 04:19:20 -0700

    final site before bug and linting fixes

--- frontend/src/Routes/Home/Components/PersonalizedHeader.tsx
+++ frontend/src/Routes/Home/Components/PersonalizedHeader.tsx
@@ -11,7 +11,7 @@ function PersonalizedHeader({name, profilePicture}: PersonalizedHeaderProps) {
         <div className="mb-0 flex items-center">
             <div>
                 <h2 className="font-semibold text-2xl mt-6">Welcome Back</h2>
-                <h3 className="leading-7 font-sans text-purple-900 font-normal">{name}</h3>
+                <h3 className="leading-7 font-sans text-purple-900 font-normal capitalize">{name}</h3>
             </div>
             <img onClick={()=> navigate("/profile")} src={profilePicture} alt="Profile icon" className="w-12 h-12 ml-auto rounded-full mr-2 mt-4" />
         </div>
